240e6d93bb9f13dc59b94fce44841416b5a03008
Sneha Rugmini <sneharugminimv@gmail.com>
2024-03-06 13:07:54 -0500

    model class Spotify.java created

---
+++ src/main/java/com/example/javaassignment13731/Spotify.java
@@ -0,0 +1,81 @@
+package main.java.com.example.javaassignment13731;
+
+public class Spotify {
+    // variables and constants
+    private int songId;
+    private String trackName;
+    private String artist;
+    private int year;
+    private int streams;
+
+//constructors
+    public Spotify(int songId, String trackName, String artist, int year, int streams) {
+        setSongId(songId);
+        setTrackName(trackName);
+        setArtist(artist);
+        setYear(year);
+        setStreams(streams);
+    }
+
+    //method - getter setter
+
+    public int getSongId() {
+        return songId;
+    }
+
+    public void setSongId(int songId) {
+        if(songId <= 0){
+            throw new IllegalArgumentException("Song ID cant be negative");
+        } else{
+        this.songId = songId;
+        }
+    }
+
+    public String getTrackName() {
+        return trackName;
+    }
+
+    public void setTrackName(String trackName) {
+        if(trackName.isBlank()){
+            throw new IllegalArgumentException("Track name cant be empty");
+        } else {
+        this.trackName = trackName;
+        }
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        if (artist.isBlank()){
+            throw new IllegalArgumentException("Artist cant be empty");
+        } else {
+        this.artist = artist;
+        }
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        if(year <= 0) {
+            throw new IllegalArgumentException("Year cant be negative");
+        } else {
+        this.year = year;
+        }
+    }
+
+    public int getStreams() {
+        return streams;
+    }
+
+    public void setStreams(int streams) {
+        if(streams <= 0){
+            throw new IllegalArgumentException("Streams cannot be negative");
+        } else {
+        this.streams = streams;
+        }
+    }
+}
